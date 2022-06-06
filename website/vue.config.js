020a3cf17e3bf28425885fc7aa400c94d5999808
tukuyo <tukuyo.dayo@gmail.com>
2022-06-06 23:07:38 +0900

    f

--- website/vue.config.js
+++ website/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "outputDir": "../dist",
-  "transpileDependencies": [
-    "vuetify"
-  ]
+    "outputDir": "../docs",
+    "transpileDependencies": [
+        "vuetify"
+    ]
 }
